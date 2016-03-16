938db4a4aa2efa3c95b92bd1e6e54ae157dd8b44
Adam Kidd <adam_kidd@hotmail.co.uk>
2016-03-16 15:52:40 +0000

    this is what i was talking for the cpp/h format. standardFunction has been changed to reflect this.

--- carCode/carCode.ino
+++ carCode/carCode.ino
@@ -1,6 +1,12 @@
+#include <Arduino.h>
+#include "standardFunctions.h"
+
+standardFunctions sf;
+
 void setup() {
   // put your setup code here, to run once:
-
+  sf.pingSensor(0);
+  
 }
 
 void loop() {
