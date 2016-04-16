c3a46346fb387cafc72c7bbb5830427e189927f3
Greg Law <Greg Law>
2016-04-16 12:56:24 +0100

    COmpleted find can method.
    Never going to be used in this form though because the servo can't turn
    360 degrees anymore. Could potentially be modified for 180 though if time
    allows.

--- carCode/scanMode.cpp
+++ carCode/scanMode.cpp
@@ -7,6 +7,11 @@
 
 static standardFunctions sf;
 
+//Turn the car to point in a certain direction
+void pointAt(int direction){
+  
+}
+
 /*
  * Given the angle from the car and distance from the car.
  * Gives array [x,y] with representative co-ordinate.
@@ -31,6 +36,25 @@ double* equationOfLine(double* coOrdOne,double* coOrdTwo){
   return equation;
 }
 
+
+ /*
+  * Returns -1 if it matches, 1 otherwise
+ */
+ int checkMatchesWall(double* point,double* wall){
+	double expectedY = wall[0]*point[0]+wall[1]; 
+	double difference = point[1]-expectedY;
+	if(difference<1){
+		difference=difference*-1;
+	}
+	if(difference<LINETOLERANCE){
+		//It's a wall
+		return -1;
+	}else{
+		//It's not a wall
+		return 1;
+	}
+ }
+
 /*
  * Given the first position of 3 values which potentially could make
  * a wall or show a can, with a no reading on either side. Will return the position of the can or -1
@@ -42,28 +66,88 @@ int wallOrCan(int* values,int start){
   double* pointThree=getCoOrd((start+2)*ANGLEINTERVAL,values[start+2]);
   double* equationOneTwo = equationOfLine(pointOne,pointTwo);
   //Expected y = mx +c
-  double pointThreeExpectedY = equationOneTwo[0]*pointThree[0]+equationOneTwo[1]; 
-  double difference = pointThree[1]-pointThreeExpectedY;
-  if(difference<1){
-    difference=difference*-1;
+  if(checkMatchesWall(pointThree,equationOneTwo)==-1){
+	  //It's a wall
+	  return -1;
+  }else{
+	  //There's a can there somewhere get closer look again.
+	  pointAt(start+1);
+	  return -2;
   }
-  if(difference<LINETOLERANCE){
-    //It's a wall
-    return -1;
-  }
-  double* equationOneThree = equationOfLine(pointOne,pointThree);  
-  double* equationTwoThree = equationOfLine(pointTwo,pointThree);
-  double origin[2] = {0,0};
-  //TODO Problem here if start-1 is less than 0
-  double* boundPointOne = getCoOrd((start-1)*ANGLEINTERVAL,UNRELIABLE);
-  double* boundPointTwo= getCoOrd((start+4)*ANGLEINTERVAL,UNRELIABLE);
-  double* boundingLineOne = equationOfLine(origin,
+ }
+ 
+ /*
+ 
+ */
+ double* formWallOrFindCan(int* values,int start){
+	 double* points[4];
+	 double* potentialLine;
+   double* answer=(double*) malloc(2*sizeof(double));
+	 double expectedY;
+	 double difference;
+	 for(int a=0;a<4;a++){
+		points[a]=getCoOrd((start+a)*ANGLEINTERVAL,values[(start+a)]);
+	 }
+	 for(int i=0;i<2;i++){		 
+		 //Gives point one
+		 //Need combination of 3 points so lowest point can only be 1 or 2
+		 for(int j=i+1;j<3;j++){
+			 //Gives point two
+			 //Needs at least one more point after it so can only be 3 at max
+			 potentialLine=equationOfLine(points[i],points[j]);
+			 for(int k=j+1;k<4;k++){
+				//Gives third point
+				//has to be 4th at max because we're looking at 4 values
+				//y=mx+c
+				if(checkMatchesWall(points[k],potentialLine)==-1){
+					//It's a wall
+					if(k==3){
+						//The first 3 made a wall look at the fourth
+						if(checkMatchesWall(points[4],potentialLine)){
+							//All of them make a wall
+							return potentialLine;
+						}else{
+							//The last one doesn't match the wall it's a can
+              answer[0]=start+3;
+              answer[1]=0;
+							return answer;
+						}
+					}else{
+						//3 of 4 made a wall need to find out which wasn't used it'll be a can.
+						if(j==2){
+							//The second value in the wall was the second point and
+							//the third point in wall was the fourth point. So the third
+							//point doesn't match the wall it must be a can, return it.
+							answer[0]=start+2;
+              answer[1]=0;
+              return answer;
+						}else{
+							if(i==2){
+								//The second and third points of the wall were the third and fourth points given
+								//So whatever one out of 1 and 2 wasn't the first point must be the can.
+								answer[0]=start;
+                answer[1]=0;
+                return answer;
+							}else{
+							  answer[0]=start+1;
+                answer[1]=0;
+                return answer;
+							}
+						}
+					}
+				}
+			 }
+		 }
+			 
+	 }
+   answer[1]=666;
+   return answer;
  }
 
 /*
- * Mark a possibility if there's two readings in a row.
+ * Mark a possibility for two readings in a row given the position of the first.
  */
-void markPossibility(){
+void markPossibility(int start){
   return;
 }
 
@@ -71,6 +155,8 @@ int findCan(){
   //Need to consider checking next value aswell if it isn't unknown (i.e. meeting the start of the loop) this gets really confusing.
   char meaning[24];
   int value[24];
+  double* wall;
+  double* result;
   int consecutiveReadings=0;
   int beforeReadingValue;
   for(int i=0;i<24;i++){
@@ -79,6 +165,7 @@ int findCan(){
     meaning[i]='U';
   }
   for(int i=0;i<24;i++){
+	  //Need to think about going back to find first no reading to start on.
     value[i]=sf.pingSensor(0);
     if(value[i]>UNRELIABLE){
       meaning[i]='N';
@@ -91,31 +178,49 @@ int findCan(){
             break;
         case 2:
           //Need to look at what parameters will be needed.
-          markPossibility();
+          markPossibility(i-2);
           break;
         case 3:
-          //Mark all as wall or head for can
+          return wallOrCan(value,i-3);
           break;
       }
       consecutiveReadings=0;
-      //wipeWall
     }else{
-      value[i]='R';
+      meaning[i]='R';
       consecutiveReadings++;
       if(consecutiveReadings==4){
         //Form wall or return can
+		result = formWallOrFindCan(value,i-3);
+		if (result[1]==666){
+			//Error just move on.
+			consecutiveReadings=0;
+		}else{
+			if(result[1]==0){
+				return result[0];
+			}else{
+				wall=result;
+			}
+		}
       }else if(consecutiveReadings>4){
         //check reading matches wall or return as can
+		if(checkMatchesWall(getCoOrd(i*ANGLEINTERVAL,value[i]),wall)==1){
+			//If the next one doesn't match the wall it's a can go for it.
+			return i;
+		}
       }
     }
     sf.turnSensor(ANGLEINTERVAL);
   }
   //Haven't found can yet so look at possibilities and walls and stuff
-  //if you still have no idea return 25 let the above level move and call this again.
+  //if you still have no idea return -1 let the above level move and call this again.
 }
 
 void scanMode::scan(standardFunctions standardFunc){
   sf=standardFunc;
+  //Need multiple responses to find can
+  //-1 edge forward try again
+  //-2 edge forward mini scan
+  //anything else point at it and return
   findCan();
   return;
 //find can
