91570684375a42d64c05e35694e49757c284343c
garryhenderson <gahenderson@sky.com>
2016-04-18 11:22:13 +0100

    commiting to master

--- carCode/standardFunctions.h
+++ carCode/standardFunctions.h
@@ -3,18 +3,22 @@
 #ifndef standardFunctions_h
 #define standardFunctions_h
 
+#include <Servo.h>
 #include <Arduino.h>
 
 
 	
 class standardFunctions{
 public:
-  int pingSensor(int pingID);
-  void turnSensor(int degrees);
+	int pingSensor(int pingID);
+  void turnServo(int degrees);
+  //void turnSensor(int degrees);
   void setupStandardFunctions();
   void turn(int direction);
   void drive(int direction);
-  void turnServo(int degrees);
+//  void turnServo(int degrees);
+//difference pls delete
 };
+
 
 #endif
